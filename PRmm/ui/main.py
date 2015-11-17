838aca47df570648083cd6e42ef6fe284d88f5d5
David Alexander <dalexander@pacificbiosciences.com>
2015-11-16 17:51:36 -0800

    WIP Remove prints

--- PRmm/ui/main.py
+++ PRmm/ui/main.py
@@ -47,9 +47,6 @@ class TraceViewer(QtGui.QMainWindow):
         if self.alnView is not None:
             # Only so this if we are showing base labels
             def xRangeChangedHandler(x):
-                print "The x-axis was changed!"
-                print self.frameInterval
-                print self.baseInterval
                 self.alnView.focus(*self.baseInterval)
             self.plot1.sigXRangeChanged.connect(xRangeChangedHandler)
 
