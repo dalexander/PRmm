0202fd94d14b9e1f299c544cd80260a0a551588f
David Alexander <dalexander@pacificbiosciences.com>
2015-11-20 10:28:01 -0800

    rsync mods

--- rsync.sh
+++ rsync.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-rsync -avx  $(pwd) login14-biofx01:/home/UNIXHOME/dalexander/Projects/git
+rsync --exclude='.git/'  --exclude build --exclude dist \
+       	-avx  $(pwd) login14-biofx01:/home/UNIXHOME/dalexander/Projects/git
