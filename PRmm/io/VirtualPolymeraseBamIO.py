942001d9864999510d27a25fe6fb2500736e25ad
David Alexander <dalexander@pacificbiosciences.com>
2015-11-22 09:12:48 -0800

    WIP

--- PRmm/io/VirtualPolymeraseBamIO.py
+++ PRmm/io/VirtualPolymeraseBamIO.py
@@ -3,11 +3,43 @@ __all__ = [ "VirtualPolymeraseBamReader" ]
 
 # don't make this harder than it needs to be.  just supply the API needed by the model
 
+from pbcore.io import IndexedBamReader
+
+def recordsStartAtZero():
+    pass
+
+def recordsAreContiguousReads():
+    pass
+
+def concatenateRecordTags():
+    pass
+
 class VirtualPolymeraseBamReader(object):
 
-    def readByZmw(self, hn):
+    def __init__(self, subreadsFname, scrapsFname=None):
+        if not subreadsFname.endswith(".subreads.bam"):
+            raise Exception, "Expecting a subreads.bam"
+        if scrapsFname is None:
+            scrapsFname = subreadsFname.replace("subreads.bam", "scraps.bam")
+        self.subreadsF = IndexedBamReader(subreadsFname)
+        self.scrapsF   = IndexedBamReader(scrapsFname)
+
+    @property
+    def hasPulses(self):
         pass
 
-    # Or maybe
-    def multialignmentByZmw(self, hn):
+    @property
+    def holeNumbers(self):
+        return self.subreadsF.holeNumber
+
+    def __getitem__(self, holenumber):
         pass
+
+
+class VirtualPolymeraseZmw(object):
+
+    def __init__(self, bamRecords):
+        pass
+
+    def readNoQC(self):
+        return self
