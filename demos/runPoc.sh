f5d2bf2b8dd3de2fb01a086208e3f940dd525713
David Alexander <dalexander@pacificbiosciences.com>
2015-12-22 18:49:57 -0800

    Fix the demo scripts

--- demos/runPoc.sh
+++
@@ -1,7 +0,0 @@
-prmm --debug \
-     /pbi/collections/sequelmilestone/3100134/0001/m54006_151021_185942.trc.h5 \
-     --bas /pbi/collections/sequelmilestone/3100134/0001/XT_nT2P_d5/m54006_151021_185942.bas.h5 \
-     --pls /pbi/collections/sequelmilestone/3100134/0001/XT_nT2P_d5/m54006_151021_185942.pls.h5 \
-     --aln /home/UNIXHOME/nechols/data/sequel/all4mer/m54006_151021_185942/PoC/aligned_PoC_ignoreHQ.cmp.h5 \
-     --hole 47776057
-
--- demos/runSims.sh
+++ demos/runSims.sh
@@ -1,6 +1,1 @@
-prmm --debug \
-	/pbi/dept/primary/sim/ffhmm/ffHmm_256Lx3C_102715_SNR-15.trc.h5 \
-     --bas /home/UNIXHOME/dalexander/Projects/git/VariantCallingReports/2015/HQ-Region-Finding/SequelT2B/Sims/ffHmm_256Lx3C_102715_SNR-15.bax.h5 \
-     --pls /home/UNIXHOME/dalexander/Projects/git/VariantCallingReports/2015/HQ-Region-Finding/SequelT2B/Sims/ffHmm_256Lx3C_102715_SNR-15.plx.h5 \
-     --hole 10944688
-
+prmm --fixture=~/Data/SimsAnalysis/fixtures.ini::T2B-bastard --hole=10944688
