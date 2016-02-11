b551872a582f4ee19a16162e11d8b9202a4fb5ec
David Alexander <dalexander@pacificbiosciences.com>
2016-02-10 20:42:43 -0800

    New dest for rsync

--- rsync.sh
+++ rsync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rsync --exclude='.git/'  --exclude build --exclude dist \
-       	-avx  $(pwd) login14-biofx01:/home/UNIXHOME/dalexander/Projects/git
+ -avx  $(pwd) login14-biofx01:/home/UNIXHOME/dalexander/Projects/rsync/
