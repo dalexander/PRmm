0600d1e1cb9614224fe8c4aaee969fd896a7a44a
David Alexander <dalexander@pacificbiosciences.com>
2016-02-10 21:43:09 -0800

    More rsync exclude

--- rsync.sh
+++ rsync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync --exclude='.git/'  --exclude build --exclude dist \
+rsync --exclude='.git/'  --exclude build --exclude dist --exclude PRmm.egg-info \
  -avx  $(pwd) login14-biofx01:/home/UNIXHOME/dalexander/Projects/rsync/
